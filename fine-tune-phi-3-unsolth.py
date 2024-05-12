d3112d9e011e78dce680a3aed03a4e811d6110d3
Le The Viet <letheviet0708@gmail.com>
2024-05-12 20:27:58 +0700

    first commit

--- fine-tune-phi-3-unsolth.py
+++ fine-tune-phi-3-unsolth.py
@@ -125,7 +125,7 @@ max_memory = round(gpu_stats.total_memory / 1024 / 1024 / 1024, 3)
 print(f"GPU = {gpu_stats.name}. Max memory = {max_memory} GB.")
 print(f"{start_gpu_memory} GB of memory reserved.")
 
-trainer_stats = trainer.train("./outputs/checkpoint-3700")
+trainer_stats = trainer.train()
 
 #@title Show final memory and time stats
 used_memory = round(torch.cuda.max_memory_reserved() / 1024 / 1024 / 1024, 3)
